c1e16644a6de34a2b50ab19c647f1003c1723816
Cilan <abraham.hamidi01@gmail.com>
2018-08-14 18:54:18 -0500

    Restructure code for production

---
+++ client/src/components/NotFound/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Are you lost?</h1>
+        <p>We can't find this page.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound',
+};
+</script>
